dadfcf041f6917e2af87a1aa743877ecb79ad2b9
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-07 10:47:43 +0300

    feat: created API for specialization

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,9 +1,11 @@
 from .curriculum import router as curriculum_router
 from .file import router as file_router
 from .department import router as department_router
+from .specialization import router as specialization_router
 
 all_routers = [
     department_router,
+    specialization_router,
     curriculum_router,
     file_router
 ]
---
+++ src/api/specialization.py
@@ -0,0 +1,44 @@
+from loguru import logger
+from fastapi import APIRouter
+
+from ..services import SpecializationService
+from ..schemas import SpecializationSchema, SpecializationCreateSchema, SpecializationUpdateSchema
+from ..utils.dependencies import UOWDependencies
+
+router = APIRouter(
+    prefix="/api/v1/specializations",
+    tags=["Specialization"],
+)
+
+
+@router.get("", response_model=list[SpecializationSchema], status_code=200)
+async def get_specializations(uow: UOWDependencies) -> list[SpecializationSchema]:
+    return await SpecializationService.get_specializations(uow)
+
+
+@router.get("/{specialization_id}", response_model=SpecializationSchema, status_code=200)
+async def get_specialization_by_id(uow: UOWDependencies, specialization_id: str) -> SpecializationSchema:
+    return await SpecializationService.get_specialization_by_id(uow, specialization_id)
+
+
+@router.post("", response_model=str, status_code=201)
+async def create_specialization(uow: UOWDependencies,
+                                specialization: SpecializationCreateSchema) -> str:
+    specialization_id = await SpecializationService.create_specialization(uow, specialization)
+    logger.success(f"Created specialization with id '{specialization_id}'")
+    return str(specialization_id)
+
+
+@router.put("/{specialization_id}", response_model=SpecializationSchema, status_code=200)
+async def edit_specialization(uow: UOWDependencies,
+                              specialization_id: str,
+                              specialization: SpecializationUpdateSchema) -> SpecializationSchema:
+    updated_specialization = await SpecializationService.edit_specialization(uow, specialization_id, specialization)
+    logger.success(f"Updated specialization with id '{specialization_id}")
+    return updated_specialization
+
+
+@router.delete("/{specialization_id}", response_model=None, status_code=204)
+async def delete_specialization(uow: UOWDependencies, specialization_id: str) -> None:
+    await SpecializationService.delete_specialization(uow, specialization_id)
+    logger.success(f"Deleted specialization with id '{specialization_id}'")
