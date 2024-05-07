22acaed4d2f32cb23f9b213871844b04e939a35c
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-07 11:27:42 +0300

    feat: created schemas for education component

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,4 +1,13 @@
 from .file import FileSchema
-from .department import DepartmentSchema, DepartmentCreateSchema, DepartmentUpdateSchema
-from .specialty import SpecialtySchema, SpecialtyCreateSchema, SpecialtyUpdateSchema
-from .specialization import SpecializationSchema, SpecializationCreateSchema, SpecializationUpdateSchema
+from .department import (DepartmentSchema,
+                         DepartmentCreateSchema,
+                         DepartmentUpdateSchema)
+from .specialty import (SpecialtySchema,
+                        SpecialtyCreateSchema,
+                        SpecialtyUpdateSchema)
+from .specialization import (SpecializationSchema,
+                             SpecializationCreateSchema,
+                             SpecializationUpdateSchema)
+from .education_component import (EducationComponentSchema,
+                                  EducationComponentCreateSchema,
+                                  EducationComponentUpdateSchema)
