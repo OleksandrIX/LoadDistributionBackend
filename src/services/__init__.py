a2779f525abb80711873e5b49558eafe25fb96b4
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-07 12:43:19 +0300

    feat: created service for study groups

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -2,3 +2,4 @@ from .department import DepartmentService
 from .specialty import SpecialtyService
 from .specialization import SpecializationService
 from .education_component import EducationComponentService
+from .study_group import StudyGroupService
