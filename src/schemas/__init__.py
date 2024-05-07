c9d5fe7ea2384305cd95d452981e94d282360aa9
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-07 10:45:29 +0300

    feat: created Specialization schemas

--- src/schemas/__init__.py
+++ src/schemas/__init__.py
@@ -1,2 +1,3 @@
 from .file import FileSchema
 from .department import DepartmentSchema, DepartmentCreateSchema, DepartmentUpdateSchema
+from .specialization import SpecializationSchema, SpecializationCreateSchema, SpecializationUpdateSchema
