885c730131e84977d345db581cd04bac004a5cdf
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-07 13:43:56 +0300

    feat: created service for academic hours

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -4,3 +4,4 @@ from .specialization import SpecializationService
 from .education_component import EducationComponentService
 from .study_group import StudyGroupService
 from .semester import SemesterService
+from .academic_hours import AcademicHoursService
