523a3f3980d161537c4875797ce6bbf2c6e5e372
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-07 09:10:15 +0300

    fix: renamed id on department_id

--- src/api/department.py
+++ src/api/department.py
@@ -16,26 +16,28 @@ async def get_departments(uow: UOWDependencies) -> list[DepartmentSchema]:
     return await DepartmentService.get_departments(uow)
 
 
-@router.get("/{id}", response_model=DepartmentSchema, status_code=200)
-async def get_department_by_id(uow: UOWDependencies, id: str) -> DepartmentSchema:
-    return await DepartmentService.get_department_by_id(uow, id)
+@router.get("/{department_id}", response_model=DepartmentSchema, status_code=200)
+async def get_department_by_id(uow: UOWDependencies, department_id: str) -> DepartmentSchema:
+    return await DepartmentService.get_department_by_id(uow, department_id)
 
 
 @router.post("", response_model=str, status_code=201)
-async def get_departments(uow: UOWDependencies, departmnet: DepartmentCreateSchema) -> str:
-    departmnet_id = await DepartmentService.create_department(uow, departmnet)
-    logger.success(f"Created department with id '{departmnet_id}'")
-    return str(departmnet_id)
+async def get_departments(uow: UOWDependencies, department: DepartmentCreateSchema) -> str:
+    department_id = await DepartmentService.create_department(uow, department)
+    logger.success(f"Created department with id '{department_id}'")
+    return str(department_id)
 
 
-@router.put("/{id}", response_model=DepartmentSchema, status_code=200)
-async def edit_department(uow: UOWDependencies, id: str, department: DepartmentUpdateSchema) -> DepartmentSchema:
-    updated_department = await DepartmentService.edit_department(uow, id, department)
-    logger.success(f"Updated department with id '{id}")
+@router.put("/{department_id}", response_model=DepartmentSchema, status_code=200)
+async def edit_department(uow: UOWDependencies,
+                          department_id: str,
+                          department: DepartmentUpdateSchema) -> DepartmentSchema:
+    updated_department = await DepartmentService.edit_department(uow, department_id, department)
+    logger.success(f"Updated department with id '{department_id}")
     return updated_department
 
 
-@router.delete("/{id}", status_code=204)
-async def get_departments(uow: UOWDependencies, id: str) -> None:
-    await DepartmentService.delete_department(uow, id)
-    logger.success(f"Deleted department with id '{id}'")
+@router.delete("/{department_id}", status_code=204)
+async def get_departments(uow: UOWDependencies, department_id: str) -> None:
+    await DepartmentService.delete_department(uow, department_id)
+    logger.success(f"Deleted department with id '{department_id}'")
