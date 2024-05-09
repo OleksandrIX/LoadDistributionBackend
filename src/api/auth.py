69145ccffe56cb45f424edce834bec0f382e6516
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-09 17:37:58 +0300

    feat: added /refresh endpoint

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,16 +1,22 @@
 from loguru import logger
 from fastapi import APIRouter, HTTPException
-from fastapi.security import OAuth2PasswordRequestForm
 
 from ..services import UserService
 from ..schemas import UserSchema, UserLoginSchema, UserRegistrationSchema, TokenSchema
 from ..utils.dependencies import UOWDependencies
-from ..utils.security import verify_password, create_token
+from ..utils.security import verify_password, create_token, verify_token
 
 router = APIRouter(
     prefix="/api/v1/auth",
     tags=["Authentication"],
 )
+
+
+@router.post("/registration", status_code=201)
+async def registration_user(uow: UOWDependencies, user: UserRegistrationSchema) -> UserSchema:
+    user: UserSchema = await UserService.create_user(uow, user)
+    logger.success(f"Created user with username: {user.username} and id: {user.id}")
+    return user
 
 
 @router.post("/login", response_model=TokenSchema, status_code=200)
@@ -19,15 +25,15 @@ async def login(uow: UOWDependencies, login_user: UserLoginSchema) -> TokenSchem
     if not verify_password(login_user.password, user.password):
         raise HTTPException(status_code=400, detail="Incorrect username or password")
     user_token = TokenSchema(
-        access_token=create_token(user.email, "access"),
-        refresh_token=create_token(user.email, "refresh")
+        access_token=create_token(user.id, "access"),
+        refresh_token=create_token(user.id, "refresh")
     )
-    logger.info(f"User '{login_user.username}' with access token '{user_token.access_token}' logged in")
+    logger.info(f"User '{user.id}' logged in")
     return user_token
 
 
-@router.post("/registration", status_code=201)
-async def registration_user(uow: UOWDependencies, user: UserRegistrationSchema) -> UserSchema:
-    user: UserSchema = await UserService.create_user(uow, user)
-    logger.success(f"Created user with username: {user.username} and id: {user.id}")
-    return user
+@router.post("/refresh", status_code=200)
+async def refresh_token(uow: UOWDependencies, refresh_token: TokenSchema) -> TokenSchema:
+    uow(...)
+    verify_token(...)
+    return refresh_token
