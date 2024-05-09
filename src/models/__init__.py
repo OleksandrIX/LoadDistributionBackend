ec994cceb54001cf73ad9febaa3bcff37a518b88
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-09 13:47:35 +0300

    feat: created initial version user model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -7,3 +7,4 @@ from .academic_hours import AcademicHoursModel
 from .academic_task import AcademicTaskModel
 from .study_group import StudyGroupModel
 from .many_to_many_tables import EducationComponentsStudyGroupsModel
+from .user import UserModel
---
+++ src/models/user.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, String
+
+from ..schemas import UserSchema
+from ..utils.database import LoadDistributionBase
+from ..utils.model import IdMixin, TimestampMixin
+
+
+class UserModel(LoadDistributionBase, IdMixin, TimestampMixin):
+    __tablename__ = "users"
+
+    username = Column(String(30), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    email = Column(String(100), nullable=False, unique=True)
+
+    def to_read_model(self) -> UserSchema:
+        return UserSchema(
+            id=self.id,
+            username=self.username,
+            password=self.password,
+            email=self.email,
+            created_at=self.created_at,
+            updated_at=self.updated_at
+        )
