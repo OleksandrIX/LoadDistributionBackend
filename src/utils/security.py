66419cc79a45e05fa36a8a5fe0e81746fa025ec2
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-10 10:53:24 +0300

    feat: created utils for security

--- src/utils/security.py
+++ src/utils/security.py
@@ -1,14 +1,14 @@
-from loguru import logger
-from jose import jwt, JWTError
 from datetime import datetime, timedelta
-from passlib.context import CryptContext
 
 from fastapi import Request
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt, JWTError
+from loguru import logger
+from passlib.context import CryptContext
 
 from ..config import security_settings
-from ..schemas import TokenPayload
 from ..exceptions import UnauthorizedException
+from ..schemas import TokenPayloadSchema
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -21,25 +21,63 @@ def verify_password(password: str, hashed_password: str) -> bool:
     return password_context.verify(password, hashed_password)
 
 
-def create_token(subject: str, type_token: str, expires_delta: int = None) -> str:
+def encode_token(subject: str, type_token: str) -> str:
+    if type_token == "access":
+        expires_delta = datetime.utcnow() + timedelta(minutes=security_settings.ACCESS_TOKEN_EXPIRE)
+        to_encode = {"exp": expires_delta, "sub": str(subject)}
+        return jwt.encode(claims=to_encode,
+                          key=security_settings.JWT_SECRET_KEY,
+                          algorithm=security_settings.ALGORITHM)
+    elif type_token == "refresh":
+        expires_delta = datetime.utcnow() + timedelta(minutes=security_settings.REFRESH_TOKEN_EXPIRE)
+        to_encode = {"exp": expires_delta, "sub": str(subject)}
+        return jwt.encode(claims=to_encode,
+                          key=security_settings.JWT_REFRESH_SECRET_KEY,
+                          algorithm=security_settings.ALGORITHM)
+    else:
+        raise UnauthorizedException(massage="Invalid type token.")
+
+
+def decode_token(token: str, type_token: str, options: dict) -> dict:
+    if type_token == "access":
+        return jwt.decode(token=token,
+                          key=security_settings.JWT_SECRET_KEY,
+                          algorithms=security_settings.ALGORITHM,
+                          options=options)
+    elif type_token == "refresh":
+        return jwt.decode(token=token,
+                          key=security_settings.JWT_REFRESH_SECRET_KEY,
+                          algorithms=security_settings.ALGORITHM,
+                          options=options)
+    else:
+        raise UnauthorizedException(massage="Invalid type token.")
+
+
+def create_token(subject: str, type_token: str) -> str:
     """
     Generates a JWT token
     :param subject: The subject of the token`
     :param type_token: The type of token (access or refresh)
-    :param expires_delta: The number of seconds that the token should expire
     :return: The JWT token
     """
-    if not expires_delta:
-        if type_token == "access":
-            expires_delta = datetime.utcnow() + timedelta(minutes=security_settings.ACCESS_TOKEN_EXPIRE)
-        elif type_token == "refresh":
-            expires_delta = datetime.utcnow() + timedelta(minutes=security_settings.REFRESH_TOKEN_EXPIRE)
-    else:
-        expires_delta = datetime.utcnow() + expires_delta
+    return encode_token(subject, type_token)
 
-    to_encode = {"exp": expires_delta, "sub": str(subject)}
-    encoded_jwt = jwt.encode(to_encode, security_settings.JWT_SECRET_KEY, security_settings.ALGORITHM)
-    return encoded_jwt
+
+def verify_token(token: str, type_token: str) -> TokenPayloadSchema:
+    """
+        Verification a JWT token
+        :param token: The JWT token to verify
+        :param type_token: The type of token (access or refresh)
+        :return: True if the token is valid, False otherwise
+        """
+    try:
+        payload = decode_token(token, type_token, options={"verify_exp": False})
+        token_payload = TokenPayloadSchema(**payload)
+        if datetime.fromtimestamp(token_payload.exp) < datetime.now():
+            raise UnauthorizedException(massage="Authorization token expired.")
+        return token_payload
+    except JWTError as err:
+        raise UnauthorizedException(massage=str(err))
 
 
 class JWTBearer(HTTPBearer):
@@ -47,6 +85,7 @@ class JWTBearer(HTTPBearer):
         super(JWTBearer, self).__init__(auto_error=False)
 
     async def __call__(self, request: Request):
+        logger.debug(request.cookies)
         credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
         if not credentials:
             raise UnauthorizedException(massage="Authorization token not found.")
@@ -54,27 +93,3 @@ class JWTBearer(HTTPBearer):
             raise UnauthorizedException(massage="Invalid authentication scheme.")
         verify_token(credentials.credentials, type_token="access")
         return credentials.credentials
-
-
-def verify_token(token: str, type_token: str) -> bool:
-    payload = None
-    try:
-        if type_token == "access":
-            payload = jwt.decode(token,
-                                 key=security_settings.JWT_SECRET_KEY,
-                                 algorithms=security_settings.ALGORITHM,
-                                 options={"verify_exp": False})
-        elif type_token == "refresh":
-            payload = jwt.decode(token,
-                                 key=security_settings.JWT_REFRESH_SECRET_KEY,
-                                 algorithms=security_settings.ALGORITHM,
-                                 options={"verify_exp": False})
-        if payload is not None:
-            token_data = TokenPayload(**payload)
-            if datetime.fromtimestamp(token_data.exp) < datetime.now():
-                raise UnauthorizedException(massage="Authorization token expired.")
-            return True
-        return False
-    except JWTError as e:
-        logger.warning(e)
-        raise UnauthorizedException(massage="Invalid authorization token.")
