659ae99b4fbbfd66e096746176b3ee37233b1cad
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-10 10:52:31 +0300

    fix: removed token schema from user schemas and edit user schemas

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -3,29 +3,18 @@ from pydantic import BaseModel, Field, EmailStr
 from ..utils.schema import IdMixinSchema, TimestampMixinSchema
 
 
-class UserBase(BaseModel):
+class UserLoginSchema(BaseModel):
     username: str = Field(..., max_length=30)
-
-
-class TokenSchema(BaseModel):
-    access_token: str
-    refresh_token: str
-
-
-class TokenPayload(BaseModel):
-    sub: str = None
-    exp: int = None
-
-
-class UserLoginSchema(UserBase):
     password: str = Field(..., min_length=8, max_length=32)
 
 
-class UserRegistrationSchema(UserBase):
+class UserRegistrationSchema(BaseModel):
+    username: str = Field(..., max_length=30)
     email: EmailStr
     password: str = Field(..., min_length=8, max_length=32)
 
 
-class UserSchema(TimestampMixinSchema, IdMixinSchema, UserBase):
+class UserSchema(TimestampMixinSchema, IdMixinSchema):
+    username: str
     email: EmailStr
     password: str
