d1d5ce49fa2605062ab2212be0de43b0d535d14e
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-14 12:40:33 +0300

    removed: removed set and delete cookies in /login, /refresh and /logout endpoints

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -31,7 +31,7 @@ async def registration_user(uow: UOWDependencies, user: UserRegistrationSchema) 
     path="/login",
     status_code=200,
     response_model=TokenSchema,
-    response_description="Created in the cookies refresh_token and logged_in"
+    response_description="Created in the cookies refresh_token"
 )
 async def login(response: Response, uow: UOWDependencies, login_user: UserLoginSchema) -> TokenSchema:
     user: UserSchema = await UserService.get_user_by_username(uow, login_user.username)
@@ -43,10 +43,6 @@ async def login(response: Response, uow: UOWDependencies, login_user: UserLoginS
                         security_settings.REFRESH_TOKEN_EXPIRE * 60,
                         security_settings.REFRESH_TOKEN_EXPIRE * 60,
                         "/", None, True, True, "lax")
-    response.set_cookie("logged_in", "True",
-                        security_settings.ACCESS_TOKEN_EXPIRE * 60,
-                        security_settings.ACCESS_TOKEN_EXPIRE * 60,
-                        "/", None, True, True, "lax")
     logger.info(f"User '{user.id}' logged in")
     return TokenSchema(access_token=access_token)
 
@@ -55,25 +51,22 @@ async def login(response: Response, uow: UOWDependencies, login_user: UserLoginS
     "/refresh",
     status_code=200,
     response_model=TokenSchema,
-    response_description="Updated in the cookies logged_in and return access token"
+    response_description="Return access token"
 )
-async def refresh_token(request: Request, response: Response, uow: UOWDependencies) -> TokenSchema:
+async def refresh_token(request: Request, uow: UOWDependencies) -> TokenSchema:
     refresh_token = request.cookies.get("refresh_token")
+    if not refresh_token:
+        raise UnauthorizedException(message="Refresh token not found")
     token_payload: TokenPayloadSchema = verify_token(refresh_token, "refresh")
     user: UserSchema = await UserService.get_user_by_id(uow, token_payload.sub)
     access_token = create_token(user.id, "access")
-    response.set_cookie("logged_in", "True",
-                        security_settings.ACCESS_TOKEN_EXPIRE * 60,
-                        security_settings.ACCESS_TOKEN_EXPIRE * 60,
-                        "/", None, True, True, "lax")
     return TokenSchema(access_token=access_token)
 
 
 @router.delete(
     path="/logout",
     status_code=204,
-    response_description="Deleted from the cookies refresh_token and logged_in"
+    response_description="Deleted from the cookies refresh_token"
 )
 async def logout(response: Response):
-    response.delete_cookie("logged_in")
     response.delete_cookie("refresh_token")
