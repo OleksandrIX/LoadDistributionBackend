41909c539ae4a00e2720643a5815e6f3767dbf45
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-19 14:25:06 +0300

    feat: created AccessControlDependencies

--- src/utils/dependencies.py
+++ src/utils/dependencies.py
@@ -2,11 +2,13 @@ from typing import Annotated
 
 from fastapi import Depends
 
-from .security import JWTBearer, get_current_user, check_is_admin
+from .access import check_is_admin, access_control
+from .security import JWTBearer, get_current_user
 from .unit_of_work import IUnitOfWork, UnitOfWork
 from ..schemas import UserSchema
 
 UOWDependencies = Annotated[IUnitOfWork, Depends(UnitOfWork)]
+CurrentUserDependencies = Annotated[UserSchema, Depends(get_current_user)]
 SecurityDependencies = Depends(JWTBearer())
 AdminDependencies = Depends(check_is_admin)
-CurrentUserDependencies = Annotated[UserSchema, Depends(get_current_user)]
+AccessControlDependencies = Depends(access_control)
