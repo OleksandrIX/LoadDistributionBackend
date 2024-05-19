8715f2f68fc4605f70c5262302f510e9871291bd
OleksandrIX <zagorenkosasha@gmail.com>
2024-05-19 14:58:17 +0300

    refactor: refactoring access functions

--- src/utils/access.py
+++ src/utils/access.py
@@ -1,7 +1,5 @@
 from json import JSONDecodeError
 
-from loguru import logger
-
 from fastapi import Request, Depends
 
 from .schema import RoleEnum
@@ -9,6 +7,10 @@ from .security import get_current_user
 from .unit_of_work import IUnitOfWork, UnitOfWork
 from ..exceptions import ForbiddenException
 from ..schemas import UserSchema
+
+
+def has_access(user_department_id: str, department_id: str) -> bool:
+    return user_department_id == department_id
 
 
 async def get_request_body(request: Request) -> dict:
@@ -19,54 +21,46 @@ async def get_request_body(request: Request) -> dict:
         return {}
 
 
-async def check_department_access(user: UserSchema) -> bool:
-    ...
-
-
-async def check_teacher_access(user: UserSchema, body: dict, uow: IUnitOfWork) -> bool:
-    department_id_from_user = user.department_id
-    department_id_from_teacher = body.get("department_id")
-
-    logger.debug(department_id_from_user)
-    logger.debug(department_id_from_teacher)
-
-
-async def access_control(request: Request,
-                         body: dict = Depends(get_request_body),
-                         uow: IUnitOfWork = Depends(UnitOfWork)) -> bool:
+async def check_access_to_teachers(request: Request, body: dict, user_department_id: str, uow: IUnitOfWork) -> bool:
     from ..services import TeacherService
 
-    is_accessible = False
+    teacher_id = request.path_params.get("teacher_id")
+    department_id = body.get("department_id")
+
+    if teacher_id:
+        if department_id:
+            return has_access(user_department_id, department_id)
+        else:
+            teacher = await TeacherService.get_teacher_by_id(uow, teacher_id)
+            return has_access(user_department_id, teacher.department_id)
+    else:
+        return has_access(user_department_id, department_id) if department_id else False
+
+
+async def access_control(
+        request: Request,
+        body: dict = Depends(get_request_body),
+        uow: IUnitOfWork = Depends(UnitOfWork)
+) -> bool:
     user: UserSchema = await get_current_user(request, uow)
     if user.role == RoleEnum.admin:
         return True
-    department_id_from_user = user.department_id
 
+    user_department_id = user.department_id
     path_parts = request.url.path.split("/")
     resource = path_parts[3]
 
     match resource:
         case "departments":
             department_id = request.path_params.get("department_id")
-            if department_id:
-                is_accessible = department_id_from_user == department_id
+            is_accessible = has_access(user_department_id, department_id) if department_id else False
         case "teachers":
-            teacher_id = request.path_params.get("teacher_id")
-            department_id_from_body = body.get("department_id")
-            if teacher_id:
-                teacher = await TeacherService.get_teacher_by_id(uow, teacher_id)
-                if department_id_from_body:
-                    is_accessible = department_id_from_user == department_id_from_body
-                else:
-                    is_accessible = department_id_from_user == teacher.department_id
-            else:
-                if department_id_from_body:
-                    is_accessible = department_id_from_user == department_id_from_body
+            is_accessible = await check_access_to_teachers(request, body, user_department_id, uow)
         case _:
             raise ForbiddenException(message="Unknown resource.")
 
     if not is_accessible:
-        raise ForbiddenException(message="You do not have access to this resource")
+        raise ForbiddenException(message="You do not have access to this resource.")
     return is_accessible
 
 
