8bfdcd7a030a0f325bc50d4802b708dc781a6be9
OleksandrIX <zagorenkosasha@gmail.com>
2024-06-12 12:16:09 +0300

    feat: added in main.py application version in FastAPI instance

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 from fastapi_pagination.utils import disable_installed_extensions_check
 
-from .config import CustomizeLogger
+from .config import CustomizeLogger, application_settings
 from .middlewares import ExceptionHandlerMiddleware, CorsMiddleware
 from .routes import all_routers
 
@@ -10,7 +10,7 @@ from .routes import all_routers
 def create_app() -> FastAPI:
     app = FastAPI(
         title="Load distribution API",
-        version="0.3.5",
+        version=application_settings.APP_VERSION,
         docs_url="/api/v1/docs"
     )
     app.logger = CustomizeLogger.make_logger()
